05af13fc806d1aabb3325499bafab588d366d21f
DDSnarr <ddsnarr@gmail.com>
2022-08-20 14:49:31 -0400

    Created is_even endpoint with specified results

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import abort
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/is_even")
+def is_even():
+    number = request.args.get('number')
 
-app.run(port=5000)
+    if number is None:
+        abort(400, "Enter number=")
+
+    if not number.isnumeric():
+        abort(400, 'Input needs to be a numeric value')
+
+    number = int(number)
+    
+    if number % 2 == 0:
+        return jsonify(result=True)
+    else:
+        return jsonify(result=False)
+
+
+
+
+app.run(port=5000, debug=True)
